944fcb954f82f476992796686c4a0ab9a8ab10a1
lshaoqin <shaoqin.lu@gmail.com>
2023-01-15 13:19:08 +0800

    added new guess method

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,4 +1,10 @@
 class GuessesController < ApplicationController
     def add
-        @guess = Guess.create('')
+        @guess = Guess.create(guess: params[:guess], users_id: params[:session_id], rounds_id: params[:round_id])
+        if @guess.save
+            render json: @guess, status: 200
+        else
+            render json: {errors: @guess.errors}, status: :unprocessable_entity
+        end
+    end
 end
